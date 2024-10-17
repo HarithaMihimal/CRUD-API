b170aae7abae69518baa3dc23764a3f90e62d040
HarithaMihimal <harithamihimal4@gmail.com>
2024-10-17 10:25:02 +0530

    Refactor root endpoint response message and add MongoDB connection

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app=  express();
 
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+
 
 
 app.get('/', (req, res) => {
 res.send('Hello World-');
+});
+
+
+mongoose.connect(`mongodb+srv://admin:admin@crud-api-backenddb.vjpkb.mongodb.net/CRUD-API?retryWrites=true&w=majority&appName=CRUD-API-BackendDB`)
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(5000, () => {
+        console.log('Server is running on port 5000');
+    });
+})
+.catch((err) => {
+    console.log('Failed to connect to MongoDB', err);
 });
