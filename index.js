2dfcc04115d91ca1a7d7edb32628c47efd961ec0
HarithaMihimal <harithamihimal4@gmail.com>
2024-10-17 09:36:23 +0530

    Add express server and route for root endpoint

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("Hello World");
-console.log("Hello World");
+const express = require('express');
+const app=  express();
+
+
+app.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
+
+
+app.get('/', (req, res) => {
+res.send('Hello World-postman');
+});
